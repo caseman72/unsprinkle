c472a0e5d2ec0d645b189b182db4f9618480011d
Casey Manion <casey@manion.com>
2021-12-25 17:52:40 -0800

    exercise(2): solution

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
+const srcToSrcSets = (src, ext) => `
+  ${src} 1x,
+  ${src.replace(/\.jpg/, `@2x.${ext}`)} 2x,
+  ${src.replace(/\.jpg/, `@3x.${ext}`)} 3x
+`;
+
 const PhotoGridItem = ({ id, src, alt, tags }) => {
   return (
     <article>
       <Anchor href={`/photos/${id}`}>
-        <Image src={src} />
+        <picture>
+          <source
+            type="image/avif"
+            srcSet={srcToSrcSets(src, "avif")}
+          />
+          <source
+            type="image/jpeg"
+            srcSet={srcToSrcSets(src, "jpg")}
+          />
+          <Image alt={alt} src={src} />
+        </picture>
       </Anchor>
       <Tags>
         {tags.map((tag) => (
@@ -28,20 +44,27 @@ const Image = styled.img`
   height: 300px;
   border-radius: 2px;
   margin-bottom: 8px;
+  object-fit: cover;
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  padding: 4px 0;
 `;
 
 const Tag = styled.li`
+  display: inline;
   padding: 4px 8px;
   background: var(--color-gray-300);
   font-size: 0.875rem;
   font-weight: 475;
   color: var(--color-gray-800);
+
+  &:not(:last-of-type) {
+     margin-right: 0.875rem;
+  }
 `;
 
 export default PhotoGridItem;
